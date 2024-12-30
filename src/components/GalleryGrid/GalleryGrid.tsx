3d272d07be6ca02ca5f9b500ff10a96e1af93b02
Eric Zhang <superezzhang@gmail.com>
2024-12-29 21:47:39 -0500

    Add array and Card object

--- src/components/GalleryGrid/GalleryGrid.tsx
+++ src/components/GalleryGrid/GalleryGrid.tsx
@@ -1,6 +1,16 @@
 import Card from "./Card/Card"
 
 function GalleryGrid(){
+  // Props for card, currently only the filepath
+  type Card = {
+    src: string
+  }
+
+  const cardData: Card[] = [
+    {src: "./public/vite.svg"}
+  ]
+  // Props for Card
+
 
   return(
     <>
